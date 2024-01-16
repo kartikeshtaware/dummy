6c69e765151861296e103b96421cffdb536a638a
kartikesh <kartikeshtaware143@gmail.com>
2024-01-16 13:49:06 +0530

    arithamatic program

---
+++ arithamatic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class arithamatic
+{
+	public static void main(String args[])
+{
+	int a=10;
+	int b =5;
+	int c;
+	
+	System.out.println("addition is = "+(a+b));
+	System.out.println("subtraction is = "+(a-b));
+	System.out.println("multiplication is =  "+(a*b));
+    System.out.println("division is = "+(a/b));
+    System.out.println("modulus is = "+(a%b));	
+}
+}	
